3fe595062179928dc56ece70c7e59b1a0b6997bc
aydinibrahimov <aydincrocusoft@gmail.com>
2023-11-04 15:37:42 +0400

    bookstore

--- src/main/java/com/aydin/bookstore/controller/AuthorController.java
+++ src/main/java/com/aydin/bookstore/controller/AuthorController.java
@@ -1,10 +1,15 @@
 package com.aydin.bookstore.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class AuthorController {
+    @GetMapping
+    public String getData(){
+        return "";
+    }
 
 }
