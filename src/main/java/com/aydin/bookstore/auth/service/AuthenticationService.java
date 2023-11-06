5aa9b8cea76f97cf8a2c039fcc12ce9e4d1aed59
aydinibrahimov <aydincrocusoft@gmail.com>
2023-11-07 01:25:46 +0400

    security.

--- src/main/java/com/aydin/bookstore/auth/service/AuthenticationService.java
+++ src/main/java/com/aydin/bookstore/auth/service/AuthenticationService.java
@@ -26,13 +26,13 @@ public class AuthenticationService {
     private final PasswordEncoder encoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
-
     private final AuthorRepository authorRepository;
     private final StudentRepository studentRepository;
 
+
     public String register(RegisterRequest request) {
-        if(userRepository.findByEmail(request.getEmail()).isPresent()){
-            throw new RuntimeException("This account already exists");
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("------------------This account already exists-----------------");
         }
         var user = User.builder()
                 .email(request.getEmail())
@@ -47,17 +47,16 @@ public class AuthenticationService {
                     .age(request.getAge())
                     .build();
             authorRepository.save(author);
-        } else if (user.getRole().toString() == Role.STUDENT.toString()) {
+            return "Successfully registered";
+        } else if (user.getRole() == Role.STUDENT) {
             Student student = Student.builder()
                     .name(request.getName())
                     .age(request.getAge())
                     .build();
-                studentRepository.save(student);
-        } else {
-            throw new IllegalArgumentException("Invalid role");
+            studentRepository.save(student);
+            return "Successfully registered";
         }
-
-        return "Successfully registered";
+        return "Registration failed !";
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
